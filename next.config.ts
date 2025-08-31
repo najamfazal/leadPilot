e25415801828de940989e4fc421c24f1102d64e3
najam edoxi <najam@edoxi.com>
2025-08-31 14:10:28 +0000

    I think there's a problem with public/icons/icon-192...file you were stu

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -26,4 +27,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClientProvider } from './client-provider';
 export const metadata: Metadata = {
   title: 'ScoreCard CRM',
   description: 'A smart Sales Pipeline Management App',
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#FFFFFF" />
       </head>
       <body className="font-body antialiased h-full">
         <ClientProvider>{children}</ClientProvider>
